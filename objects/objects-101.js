892df24ad68aa7eca9ae1d2c934984d45e18bc31
KaoriIzutani <41350890+KaoriIzutani@users.noreply.github.com>
2020-04-13 21:59:23 -0700

    Update objects-101.js
    Review of creating/modifying objects

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -1,34 +1,37 @@
 let myBook = {
-    title: 'Cracking the Coding Interview', 
-    author: 'Gayle Laakman McDowell',
+    title: 'Web Dev Playbook', 
+    author: 'Austin Beaufort',
     pageCount: 326
 }
 
-//console.log(myBook.title) The 'myBook.title' is called a dot notation.
+console.log(myBook.title) //The 'myBook.title' is called a dot notation.
     /*To start defining an object, just type {}. No keyword before the curly braces
 is needed. Inside the curly braces, you can define the object's properties (pieces of 
 data stored on an object). Make sure to separate each property with a comma (except 
 for the last property, doesn't need a comma).*/
-/* console.log(`${myBook.title} by ${myBook.author}`) /*You can also use template strings 
-to combine objects and properties*/
+// console.log(`${myBook.title} by ${myBook.author}`) /*You can also use template strings 
+// to combine objects and properties*/
 
-myBook.title = 'Cracking the Tech Career' /*You can change an object's property by 
-reassigning the specific dot notation
-console.log(`${myBook.title} by ${myBook.author}`)*/
+//  myBook.title = 'Back End Playbook' /*You can change an object's property by 
+// reassigning the specific dot notation*/
+//  console.log(`${myBook.title} by ${myBook.author}`)
 
-/*Challenge time. Create an object of a person. Properties include name, age and 
-location. Make your template string be 'Kaori is 42 and lives in Vancouver.' Then 
-modify the age by 1.*/
+// /*Challenge time. Create an object of a person. Properties include name, age and 
+// location. Make your template string be 'Kaori is 42 and lives in Vancouver.' Then 
+// modify the age by 1.*/
 
-let newProfile = {
-    name: 'Kaori',
-    age: 42,
-    location: 'Vancouver'
+let myProfile = {
+    name: 'Francine Yoshimura',
+    age: 45,
+    location: 'Laguna Beach'
 }
+myProfile.age = myProfile.age + 1
 
-console.log(`${newProfile.name} is ${newProfile.age} and lives in ${newProfile.location}.`)
-/*When typing out your template string, it's not enough to state the property. You need 
-to prefix it with the object's name as well.*/
-newProfile.age = newProfile.age + 1 /*You can add stored numbers in objects, just like 
-in functions*/
-console.log(`${newProfile.name} is ${newProfile.age} and lives in ${newProfile.location}.`)
+ console.log(`${myProfile.name} is ${myProfile.age} and lives in ${myProfile.location}.`)
+ 
+ /*When typing out your template string (also you type curly braces, not "(" before your 
+ dot notation). it's not enough to state the property. You need to prefix it with the 
+ object's name as well.*/
+// newProfile.age = newProfile.age + 1 /*You can add stored numbers in objects, just like 
+// in functions*/
+//console.log(`${newProfile.name} is ${newProfile.age} and lives in ${newProfile.location}.`)
