1e6e7f642a782776b43f86f3b4ea2c624402edf5
KaoriIzutani <41350890+KaoriIzutani@users.noreply.github.com>
2020-08-14 17:19:22 -0700

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,11 +1,11 @@
 x = 10
-# y = 20
-# print(x + y)
+y = 20
+print(x + y)
 
 # if x == 100:
      # # print("x is 100") # Always indent four spaces on the next space for readability and 
-	 # # # # syntax reasons. Ctrl +K in Notepad ++ to multi-line comment out. Ctrl + Shift + K 
-	 # # # # to uncomment.
+	 # # syntax reasons. Ctrl +K in Notepad ++ to multi-line comment out. Ctrl + Shift + K 
+	 # # to uncomment.
 
     # elif x == 99:
     # print("x is 99")
@@ -23,7 +23,7 @@ else:
    print("x is odd")
    
 
-# for loops in Python
+# For loops in Python
 for i in range(0, 100):
     print(i)
     print(i + 1000) 
@@ -64,4 +64,19 @@ while True:
     break # If you don't put a break statement here, you will get an infinite loop.
 
 
+def my_fun(x): 
+    # print("Hi!")
+    # print("Boo-boo head")
+    # print(1 + 1)
+    print(x)
+	
+my_fun(100)
 
+# for i in range(100): # This for loop allows you to call the function 100 times without needing 
+# # to call it 100 {or how many times you define it inside the parenthesis} times. 
+    # my_fun()
+
+    
+	
+	
+    
