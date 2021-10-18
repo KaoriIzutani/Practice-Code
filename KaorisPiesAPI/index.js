3b432fe2e837a8e0a328a301f94516be61387152
KaoriIzutani <41350890+KaoriIzutani@users.noreply.github.com>
2021-10-17 22:32:35 -0700

    Ch.3 Search for Data practice

--- KaorisPiesAPI/index.js
+++ KaorisPiesAPI/index.js
@@ -1,5 +1,5 @@
 //Bring in the express server and create app
-let express = require('express'); /* The require()function goes in and brings in libraries/modules in the
+let express = require('express'); /* The require() function goes in and brings in libraries/modules in the
 node.js path*/
 let app = express(); /* express() function creates the Express app. Many other objects are created from 
 this object.*/
@@ -25,10 +25,50 @@ router.get('/', function (req, res, next){
   next(err); //This function throws an exception error message.
 });
 });
+// Create GET/search?id=n&name=str to search for pies by 'id' & or 'name'.
+/* router.get('/search', function (req, res, next) {
+  let searchObject = {
+    "id": req.query.id,
+    "name": req.query.name
+
+  }; */
+
+/* Create a 2nd router.get to return a single pie. The'/:id' is a parameter that defines an argument that
+is coming into this endpoint. Ex: http://localhost:5000/api/1. The 1 mapped at the end is the id parameter. */
+router.get('/:id', function (req, res, next) {
+  pieRepo.getById(req.params.id, function(data) {
+    if(data) {
+      res.json( {
+        "status": 200,
+        "statusText": "OK",
+        "message": "Retrieved a single pie",
+        "data": data
+      });
+    }
+    else {
+      res.status(404).json({
+        "status": 404,
+        "statusText": "Not Found",
+        "message": "The requested pie '" + req.params.id + " could not be found.",
+        "error": {
+          "code": "NOT_FOUND",
+          "message": "The requested pie '" + req.params.id + " could not be found."
+        }
+      });
+    }
+  }, function(err) {
+    next(err);
+  });
+
+});
+
+/* req.params.id is an object that takes the id #, passes it in a function with a (data) parameter and if there
+is data, a single pie is retrieved. Else, an error message shows up. A good practice is to always put in 
+some sort or "error" property for status 404 messages.*/
+
 // 1st parameter: '/'. means if someone comes into this node/endpoint, please go to the part in {}
 // 2nd parameter: A function with the objects [request], [response], and [next]
  
-
 // Configure router so all routes are prefixed with /api/v1
 app.use('/api', router);
 /* use is a function that adds something to the website's route. In this example, this adds /api to 
--- KaorisPiesAPI/repos/pieRepo.js
+++ KaorisPiesAPI/repos/pieRepo.js
@@ -9,6 +9,7 @@
 }; Instead of hard coding in data, we'll pull data instead from the pies.json object. */
 
 let fs = require('fs'); // "fs" is a built-in node module that knows how to read files.
+const { resolve } = require('path');
 const FILE_NAME = './assets/pies.json';
 
 let pieRepo = {
@@ -21,10 +22,50 @@ let pieRepo = {
         resolve(JSON.parse(data)); // Here, you're grabbing "data" and converting or parsing it into JSON
 
       }
-      
+    
     });
+  },
+  getById: function (id, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else { let pie = JSON.parse(data).find(p => p.id == id);
+        resolve(pie); /* The else statement says to find a JSON object to parse. With arrow notation, 
+        you're telling the program to go through each pie and find a pie equal to an id that user 
+        passed into the getByID function. (The # is passed or entered at the end of 
+        https://localhost:5000/api/__(the ID #)/ The result is either a pie will pull up or it will be null.*/
+        
+              
+      }
+    });
+  },
+  search: function (searchObject, resolve, reject) {
+    fs.readFile(FILE_NAME, function (err, data) {
+      if (err) {
+        reject(err);
+      }
+      else {
+        let pies = JSON.parse(data);
+       //Perform search
+       if (searchObject) {
+         /*Sample search object
+        let searchObject = {
+          "id": 1,
+          "name": 'A'
+        };*/
+        pies = pies.filter(
+          p => (searchObject.id ? p.id == searchObject.id: true) &&
+           (searchObject.name ? p.name.toLowerCase().indexOf(searchObject.name.toLowerCase()) >= 0: true));
+    }
+/* The first part of the filter function says does the searchObject.id have some sort of value? If yes, then we
+check on if p.id is equal to searchObject.id and if so, return true. And is there a value for searchObject.name?
+If yes, a case insensitive search is performed. Then it checks if the p.name is within the index of searchObject.
+name.toLowerCase. If the value is >=0, then there is a match */
+resolve(pies);
   }
-  };
-
+}); 
+  }
+};
 
 module.exports = pieRepo;
